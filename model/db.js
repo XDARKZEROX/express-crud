deaa5baf5273b4ec4aa3ce7e6ed6f226ceb91913
Alexander Guzman <a.guzman@costamar.com>
2015-12-21 10:00:50 -0500

    Se actualizaron las dependencias y el bean de passenger, se agregaron comentarios adicionales

--- model/db.js
+++ model/db.js
@@ -1,2 +1,6 @@
+//Conexión a mongodb
+//Si quisieramos chequear las dependencias que no estan
+//actualizadas usaremos el comando npm outdated
+
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/nodewebappdb');
+mongoose.connect('mongodb://localhost/mandrillocal');
--- model/passengers.js
+++ model/passengers.js
@@ -9,6 +9,7 @@ var passengerSchema = new mongoose.Schema({
   numberOfDocument: String,
   documentType: String,
   bookingCode: String
+  //En caso de ser un objeto
   //dob: { type: Date, default: Date.now },
 });
 mongoose.model('passenger', passengerSchema);
